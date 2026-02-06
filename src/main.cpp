a9d58d2d00297e04ab1d5ff2a6280b4b524eb8c6
Guilherme GELMI DE FREITAS SALVO <guilherme.gelmi@ensta-paris.fr>
2026-02-06 12:55:22 +0100

    feat: main created

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,9 @@
 #include "looper.h"
 
 int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cout << "nLoops argument missing" << std::endl;
+    }
+    double nLoops = (double) atoi(argv[1]);
+    Looper loop;
 };
